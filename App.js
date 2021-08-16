58f78cf466828d60d959d76a288378728516a929
yauravy <yauravy@gmail.com>
2021-08-16 23:57:08 +0200

    components

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Text, View, SafeAreaView, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+
+// 1) Set up redux
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1}}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}>
+        <Stack.Navigator>
+          <Stack.Screen
+             name= 'HomeScreen' component={HomeScreen} options={{ headerShown: false,}} />
+
+        <Stack.Screen
+             name= 'MapScreen' component={MapScreen} options={{ headerShown: false,}} />
+        </Stack.Navigator>
+        </KeyboardAvoidingView>
+      </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
